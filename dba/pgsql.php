370d699df9ecaa5e5b87f1421f44621db1fbadc1
Felipe Batista <feljava32@gmail.com>
2024-09-25 16:54:42 -0300

    pdo

--- dba/pgsql.php
+++ dba/pgsql.php
@@ -3,6 +3,8 @@
 $conn = pg_connect('host=localhost port=5432 dbname=postgres
  user=postgres password=fel123');
 
- pg_query($conn,"INSERT INTO famosos (codigo,nome) VALUES (1,'Teste01')");
+ pg_query($conn,"INSERT INTO famosos (codigo,nome) VALUES (4,'Teste04')");
+ pg_query($conn,"INSERT INTO famosos (codigo,nome) VALUES (2,'Teste02')");
+ pg_query($conn,"INSERT INTO famosos (codigo,nome) VALUES (3,'Teste03')");
 
  pg_close($conn);
