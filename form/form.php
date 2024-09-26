499fa5e719b35cca596d478315e60756cc3fc2fc
Felipe Batista <feljava32@gmail.com>
2024-09-26 16:41:37 -0300

    form

---
+++ form/form.php
@@ -0,0 +1,22 @@
+<html>
+<head>  
+    <meta charset="utf-8">
+    <title>Cadastro de Pessoa</title> 
+</head>
+<body>
+    <form enctype="multipart/form-data" method="post" action="form_save.php">
+
+     <label>Codigo</label>
+     <input name="id" readonly="1" type="text" style="width:30%">
+     <label>Nome</label>
+     <label>Endereço</label>
+     <label>Bairro</label>
+     <label>Tel</label>
+     <label>Email</label>
+     <label>Cidade</label>
+ 
+
+
+    </form>    
+</body>
+</html>
