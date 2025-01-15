bb8f8d9dc78c8af1c311d667d1d3d26dfc27422b
Felipe Batista <feljava32@gmail.com>
2025-01-15 16:55:57 -0300

    fazendo datagrid

---
+++ mod/App/Components/Widgets/Datagrid.php
@@ -0,0 +1,60 @@
+<?php 
+ namespace Widgets;
+
+use Controller\ActionInterface;
+
+class Datagrid {
+
+   private  $columms;
+   private  $actions;
+   private  $items;
+
+         public function __construct()
+         {    
+              $this->columms = [];
+              $this->actions = [];
+              $this->items = [];
+            
+         }
+
+
+     public function addColumn($object) {
+
+        $this->columms[] = $object;
+     }
+
+     public function addAction($label, ActionInterface $action,$field, $image = null ) {
+
+        $this->actions = ['label' => $label,'action' => $action , 'field' => $field , 'image' => $image];
+     }
+
+     public function addItem($object) {
+
+        $this->items[] = $object;
+     }
+
+
+     public function getColumns() {
+
+         return $this->columms;
+     }
+
+     public function getActions() {
+
+        return $this->actions;
+     }
+
+     public function getItems() {
+
+        return $this->items;
+     }
+
+     public function clear() {
+
+        $this->items = [];
+     }
+   
+      
+
+
+}
