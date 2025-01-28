cc7a955ecddb8f59c3ba96a763b28badcbbbc48f
Felipe Batista <feljava32@gmail.com>
2025-01-28 17:00:48 -0300

    template

--- AppFinal/App/index.php
+++ AppFinal/App/index.php
@@ -8,6 +8,9 @@ echo '<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/
 <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>';
 
 
+
+
+
 $class = $_GET['class'];
 
 if ($_GET) 
