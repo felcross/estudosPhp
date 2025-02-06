b62614ffb254be5f0a5d88566ceb8699c8f72363
felipe <feljava32@gmail.com>
2025-02-05 22:12:07 -0300

    form cliente 2

--- AppFinal/App/Page/FormPessoa.php
+++ AppFinal/App/Page/FormPessoa.php
@@ -1,4 +1,5 @@
-<?php 
+<?php
+
 namespace Page;
 
 use Components\Widgets\Form;
@@ -17,121 +18,111 @@ use Model\Grupo;
 use Exception;
 use Model\Pessoa;
 
-class FormPessoa extends PageControl 
+class FormPessoa extends PageControl
 {
-     private $form;
-     public function __construct()
-     {  
-            parent::__construct();
+   private $form;
+   public function __construct()
+   {
+      parent::__construct();
 
-            $this->form = new FormWrapper(new Form('Form_cli'));
-            $this->form->setTitle('Cadastro de Clientes');
+      $this->form = new FormWrapper(new Form('Form_cli'));
+      $this->form->setTitle('Cadastro de Clientes');
 
-            $id = new Entry('id');
-            $id->setEditable(false);
-            $nome = new Entry('nome');
-            $endereco = new Entry('endereco');
-            $bairro = new Entry('bairro');
-            $tel = new Entry('telefone');
-            $email = new Entry('email');
-            $cidade = new Combo('id_cidade');
-            $grupo = new CheckGroup('id_grupo');
-            $grupo->setLayout('horizontal');
+      $id = new Entry('id');
+      $id->setEditable(false);
+      $nome = new Entry('nome');
+      $endereco = new Entry('endereco');
+      $bairro = new Entry('bairro');
+      $tel = new Entry('telefone');
+      $email = new Entry('email');
+      $cidade = new Combo('id_cidade');
+      $grupo = new CheckGroup('id_grupo');
+      $grupo->setLayout('horizontal');
 
-             Transaction::open('configLoja');
-             $repository = new Repository('cidade');
-             $cidades = $repository->all();
-             $itens = array();
+      Transaction::open('configCasa2');
+      $repository = new Repository('cidade');
+      $cidades = $repository->all();
+      $itens = array();
 
-              foreach( $cidades as $obj_cidade)
-               {      //indice                  // valor 
-               $itens[$obj_cidade->id] =  $obj_cidade->nome;
-                }
-
-             $cidade->addItems($itens);
-             
-             $repository = new Repository('Grupo');
-             $grupos = $repository->all();
-             $itensGrupos = array();
-
-             foreach( $grupos as $obj_grupo)
-             {      //indice                  // valor 
-                $itensGrupos[$obj_grupo->id] =  $obj_grupo->nome;
-             }
-
-             $grupo->addItems($itensGrupos);
-
-            
-            
-            Transaction::close();
-            
-       
-           
-     
-         $this->form->addField( 'Nome',$nome,'300px');
-         $this->form->addField('Email',$email,'300px');
-         $this->form->addField('Endereço',$endereco,'300px');
-         $this->form->addField('Bairro',$bairro,'300px');
-         $this->form->addField('Telefone',$tel,'300px');
-         $this->form->addField('Cidade',$cidade,'300px');
-         $this->form->addField('Grupo',$grupo,'300px');
-        
-        
-         
-           $this->form->addAction('Enviar', new Action([$this,'onSave']));
-   
-            parent::add($this->form);
-
-    }
-
-
-    public function onSave() 
-      {   
-           try{
-                 Transaction::open('config');
-        
-            
-
-
-                 Transaction::close();
-
-
-           }
-           catch(Exception $e){
-
-              new Message('error', $e->getMessage());
-           }
-         
+      foreach ($cidades as $obj_cidade) {      //indice                  // valor 
+         $itens[$obj_cidade->id] =  $obj_cidade->nome;
       }
 
+      $cidade->addItems($itens);
 
-      public function onEdit($param) 
-      {      print 'esse é o id' . $param['id'];
-           try{
+      $repository = new Repository('Grupo');
+      $grupos = $repository->all();
+      $itensGrupos = array();
 
-                   Transaction::open('config');
-                   $id = isset($param['id'])? $param['id'] : null;
-                   $p1 = Pessoa::find($id);
-                      
-                     $this->form->setData($p1);
-                     
-        
-                   
-                     Transaction::close();
-
-                   
-            
-
-
-           }
-           catch(Exception $e){
-
-              new Message('error', $e->getMessage());
-              Transaction::rollback();
-           }
-         
+      foreach ($grupos as $obj_grupo) {      //indice                  // valor 
+         $itensGrupos[$obj_grupo->id] =  $obj_grupo->nome;
       }
 
+      $grupo->addItems($itensGrupos);
 
 
+
+      Transaction::close();
+
+
+
+
+      $this->form->addField('Nome', $nome, '300px');
+      $this->form->addField('Email', $email, '300px');
+      $this->form->addField('Endereço', $endereco, '300px');
+      $this->form->addField('Bairro', $bairro, '300px');
+      $this->form->addField('Telefone', $tel, '300px');
+      $this->form->addField('Cidade', $cidade, '300px');
+      $this->form->addField('Grupo', $grupo, '300px');
+
+
+
+      $this->form->addAction('Enviar', new Action([$this, 'onSave']));
+
+      parent::add($this->form);
+   }
+
+
+   public function onSave()
+   {
+      try {
+         Transaction::open('configCasa2');
+
+
+
+
+         Transaction::close();
+      } catch (Exception $e) {
+
+         new Message('error', $e->getMessage());
+      }
+   }
+
+
+   public function onEdit($param)
+   {
+      try {
+
+         
+         $id = isset($param['id']) ? $param['id'] : null;
+         Transaction::open('configCasa2');
+         $p1 = Pessoa::find($id);
+
+         if ($p1) {
+            
+            $p1->id_grupo = $p1->getIdsGrupos();
+            $this->form->setData($p1);
+         }
+
+
+
+
+
+         Transaction::close();
+      } catch (Exception $e) {
+
+         new Message('error', $e->getMessage());
+         Transaction::rollback();
+      }
+   }
 }
