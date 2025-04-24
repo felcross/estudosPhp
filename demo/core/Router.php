cb9c84e1fe0cbb4e42f1a8f8e968e9383e383942
felcross <feljava32@gmail.com>
2025-04-24 16:59:56 -0300

    ajustes em rotas

--- demo/core/Router.php
+++ demo/core/Router.php
@@ -4,54 +4,45 @@ namespace Core;
 
 class Router 
 {    public $routes = [];
+   
+   public function router($uri, $method)
+   {
+       foreach($this->routes as $route)
+       {
+           if($route['uri'] == $uri && $route['method'] == strtoupper($method))
+           {
+               return require $_SERVER['DOCUMENT_ROOT'] . '/' . 'demo/' . $route['controller'];
+           }
+       }
+       
+       // Se chegou aqui, significa que nenhuma rota foi encontrada
+       http_response_code(404);
+       echo 'Página não encontrada!';
+   }
 
-    public function router($uri, $method)       
+
+     public function add($method,$uri,$controller): void       
      {
-         foreach($this->routes as $route)
-         {   
-             if($route['uri'] == $uri && $route['method'] == strtoupper($method)) 
-             {
-                // var_dump($_SERVER['DOCUMENT_ROOT'] . '/' . 'demo/' . $route['controller']);
-
-                return require $_SERVER['DOCUMENT_ROOT'] . '/' . 'demo/' . $route['controller'];
-             } else
-             
-             {
-                http_response_code(404);
-                echo 'Pagina errada!!!';
-             }
-
-         }
+      $this->routes[] = compact('method','uri','controller');
      }
+
 
    
 
      public function get($uri,$controller): void       
      {
-       $this->routes[] =  
-       ['uri'=> $uri,
-       'controller'=> $controller,
-       'method'=> 'GET'
-       ];
+       $this->add('GET',$uri,$controller );
      }
 
      public function post($uri,$controller)
      {
-        $this->routes[] =  
-        ['uri'=> $uri,
-        'controller'=> $controller,
-        'method'=> 'POST'
-        ];
+      $this->add('POST',$uri,$controller );
 
      }
 
      public function delete($uri,$controller)
      {
-        $this->routes[] =  
-        ['uri'=> $uri,
-        'controller'=> $controller,
-        'method'=> 'DELETE'
-        ];
+      $this->add('DELETE',$uri,$controller );
 
      }
 
--- demo/routes.php
+++ demo/routes.php
@@ -2,15 +2,7 @@
 
 
 
-// return  [
 
-//     ''=> 'controllers/home.php',
-//     'contac'=> 'controllers/contact.php',
-//     'about'=> 'controllers/about.php',
-//     'notes'=> 'controllers/notes/show.php',
-//     'notes/create'=> 'controllers/notes/create.php',
-//     'note'=> 'controllers/notes/index.php',
-// ];
 
 $router->get('', 'controllers/home.php');
 $router->get('contac', 'controllers/contact.php');
